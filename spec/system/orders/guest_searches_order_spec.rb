8f909c41c3e9f0ac6a7b09e60bdb0b6521a899f2
Raphaelly V <valiengo.raphaelly@yahoo.com>
2022-10-05 00:05:28 -0300

    Alteração dos campos mostrados com a busca pela ordem de serviço

--- spec/system/orders/guest_searches_order_spec.rb
+++ spec/system/orders/guest_searches_order_spec.rb
@@ -17,7 +17,7 @@ describe 'Visitante procura por ondem de serviço' do
     first_order = Order.create!(delivery_address: 'Rua das Oliveiras, 15', delivery_city: 'São Gonçalo', delivery_state: 'RJ', 
                                 delivery_postal_code: '28200000', recipient: 'Marta Silva', recipient_cpf: '00000000000',
                                 recipient_email: 'martae@email.com', recipient_phone_number: '00000000000', 
-                                pick_up_address: 'Estrada do Porto, 70', pick_up_city: 'Santo André', pick_up_state: 'SP', 
+                                pick_up_address: 'Estrada do Trem, 70', pick_up_city: 'Santo André', pick_up_state: 'SP', 
                                 pick_up_postal_code: '30000000', sku: 'TV40P-SAMSUNG-XPTO90', height: 60, width: 40, length: 100, 
                                 weight: 300, distance: 1_200)
 
@@ -36,12 +36,10 @@ describe 'Visitante procura por ondem de serviço' do
     # Assert
     expect(page).to have_content "Resultado da Busca por #{second_order.tracking_code}"
     expect(page).to have_content "Status: Pendente"
-    expect(page).to have_content "SKU: TV32P-SAMSUNG-XPTO90"
-    expect(page).not_to have_content "SKU: TV40P-SAMSUNG-XPTO90"
-    expect(page).to have_content 'Destinatário: Denise Silva'
-    expect(page).not_to have_content 'Destinatário: Marta Silva'
     expect(page).to have_content 'Local de Entrega: Rua das Palmeiras, 13 - Rio de Janeiro - RJ'
     expect(page).not_to have_content 'Local de Entrega: Rua das Oliveiras, 15 - São Gonçalo - RJ'
+    expect(page).to have_content 'Local de Retirada: Estrada do Porto, 70 - São Paulo - SP'
+    expect(page).not_to have_content 'Local de Retirada: Estrada do Trem, 70 - Santo André - SP'
   end
 
   it ' e não encontra o pedido' do
